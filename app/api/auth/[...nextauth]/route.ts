a295f731a9107b5f3c6a6ad8a16457685cf4597b
Clayclay <mariepolmontiel@gmail.com>
2024-06-26 13:27:17 +0200

    prisma remit ds route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,16 +2,11 @@
 //import NextAuth, { NextAuthOptions } from "next-auth";
 //import { authOptions } from "@/utils/authoptions";
 
-/*
 import  { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth";*/
+import NextAuth from "next-auth/next";
 
-import NextAuth from 'next-auth'
 
-//import authOptions from '../../../../lib/authOptions'
-import { authOptions } from '../../../../lib/auth';
-
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+//import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "../../../../lib/prisma";
 
@@ -19,15 +14,46 @@ import Credentials from "next-auth/providers/credentials";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 
-//import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from "next-auth/providers/google";
 
 
 
-const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+//export  remove the export
+const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      httpOptions: {
+        timeout: 10000, // wait for response time, because the local environment often login timeout, so change this configuration
+      }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID  as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET  as string,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET, // required for production environments
+  callbacks: {
+    // triggered by getSession and useSession calls
+    // documents https://next-auth.js.org/configuration/callbacks
+    async session({ session, user }) {
+      if (user.id && session?.user) {
+        session.user.userId   = user.id;
+        
+      }
+      return session;
+    }
+  }
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
 
 /*
 Behind the scenes, this creates all the relevant OAuth API routes within /api/auth/* so that auth API requests to:
