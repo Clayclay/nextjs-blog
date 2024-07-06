0d531385df6c88900350097f2a1abae05526ac18
Clayclay <mariepolmontiel@gmail.com>
2024-07-06 10:14:19 +0200

    Erreur delete de Main.js

---
+++ components/Main.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+//import Markdown from './Markdown';
+import Markdown from 'markdown-to-jsx'
+
+
+function Main(props) {
+
+  const { posts, title } = props;
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+
+    
+      { posts.map((post) => (
+
+        <Markdown  className="markdown"  key={post.id}  >
+          {post.content.toString()}
+        </Markdown>
+      )) }
+
+    </Grid>
+  );
+}
+
+Main.propTypes = {
+  //posts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  posts : PropTypes.arrayOf(Object).isRequired,
+  title: PropTypes.string.isRequired,
+  
+};
+
+export default Main;
+
+
+
+
+
+
+
+
+
+     
